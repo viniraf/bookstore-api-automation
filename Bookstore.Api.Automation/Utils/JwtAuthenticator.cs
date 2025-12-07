97ad4c9b347f21a46c620570521390280405b866
viniraf <vrmb.pro@gmail.com>
2025-12-07 20:57:36 -0300

    feat(auth): implement JwtAuthenticator to attach bearer token automatically

---
+++ Bookstore.Api.Automation/Utils/JwtAuthenticator.cs
@@ -0,0 +1,21 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+
+namespace Bookstore.Api.Automation.Utils
+{
+    public class JwtAuthenticator : IAuthenticator
+    {
+        private readonly string _token;
+
+        public JwtAuthenticator(string token)
+        {
+            _token = token;
+        }
+
+        public ValueTask Authenticate(IRestClient client, RestRequest request)
+        {
+            request.AddOrUpdateHeader("Authorization", $"Bearer {_token}");
+            return ValueTask.CompletedTask;
+        }
+    }
+}

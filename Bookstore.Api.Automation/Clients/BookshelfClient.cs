85743b8a6bc2578336394a3b585914198a42782c
viniraf <vrmb.pro@gmail.com>
2025-12-09 17:27:06 -0300

    feat(client): add AddBook support to BookshelfClient

--- Bookstore.Api.Automation/Clients/BookshelfClient.cs
+++ Bookstore.Api.Automation/Clients/BookshelfClient.cs
@@ -1,11 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Bookstore.Api.Automation.Models.Bookshelf;
+using Bookstore.Api.Automation.Utils;
+using RestSharp;
 
 namespace Bookstore.Api.Automation.Clients
 {
     public class BookshelfClient
     {
+        private readonly RestClient _client;
 
+        public BookshelfClient(string baseUrl, string token)
+        {
+            _client = RestClientFactory.Create(baseUrl, token);
+        }
+
+        public async Task<RestResponse> ClearUserBooksAsync(string userId)
+        {
+            var request = new RestRequest("/Books", Method.Delete);
+            request.AddQueryParameter("UserId", userId);
+
+            return await _client.ExecuteAsync(request);
+        }
+
+        public async Task<RestResponse> AddBookAsync(AddBookRequest request)
+        {
+            RestRequest restRequest = new RestRequest("/Books", Method.Post);
+
+            restRequest.AddJsonBody(request);
+
+            RestResponse restResponse = await _client.ExecuteAsync(restRequest);
+
+            return restResponse;
+        }
     }
 }
